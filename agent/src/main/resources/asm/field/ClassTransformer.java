512827696fc81d3b5d5e8862a4924bf47d16eba2
MirekSz <gmail>
2020-04-03 09:36:45 +0200

    fieldadd

---
+++ agent/src/main/resources/asm/field/ClassTransformer.java
@@ -0,0 +1,179 @@
+
+package com.apec;
+
+import java.io.PrintWriter;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.reflect.Modifier;
+import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.util.TraceClassVisitor;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtBehavior;
+import javassist.CtClass;
+import javassist.LoaderClassPath;
+import javassist.NotFoundException;
+
+public class ClassTransformer implements ClassFileTransformer {
+
+	private static final List<String> SKIP = new ArrayList();
+	static {
+		SKIP.add("ImportServiceImpl");
+	}
+
+	private static boolean INITIALIZED = false;
+	private ClassLoader contextClassLoader;
+
+	private boolean skip(final String className) {
+		for (String toSkip : SKIP) {
+			if (className.contains(toSkip)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public byte[] transform(final ClassLoader loader, final String className, final Class<?> classBeingRedefined,
+			final ProtectionDomain protectionDomain, final byte[] classfileBuffer) throws IllegalClassFormatException {
+		if (loader.toString().contains("next-url-module-loader")) {
+			return classfileBuffer;
+		}
+		ClassPool pool = ClassPool.getDefault();
+		if (!INITIALIZED) {
+			contextClassLoader = Thread.currentThread().getContextClassLoader();
+			LoaderClassPath loaderClassPath = new LoaderClassPath(contextClassLoader);
+			pool.appendClassPath(loaderClassPath);
+			pool.importPackage("agent.ProfileSession");
+			INITIALIZED = true;
+		}
+		if (contextClassLoader != Thread.currentThread().getContextClassLoader()) {
+			contextClassLoader = Thread.currentThread().getContextClassLoader();
+			LoaderClassPath loaderClassPath = new LoaderClassPath(contextClassLoader);
+			pool.appendClassPath(loaderClassPath);
+		}
+		if (!className.startsWith("pl")) {
+			return classfileBuffer;
+		}
+		if (className.contains("/client/")) {
+			return classfileBuffer;
+		}
+		if (skip(className)) {
+			return classfileBuffer;
+		}
+		if (className.contains("asen") && className.contains("FrontControllerImpl")) {
+			return transform(classfileBuffer, "executeImpl", className, false);
+		}
+		if (className.contains("asen") && className.contains("GroovyScriptExecutor")) {
+			return transform(classfileBuffer, "execute", className, false);
+		}
+		if (className.contains("asen") && className.contains("DatabaseAccessServiceImpl")) {
+			return transform(classfileBuffer, "executeQuery", className, true);
+		}
+		if (className.contains("asen") && className.contains("QueryExecutorServiceImpl")) {
+			return transform(classfileBuffer, Arrays.asList("executeQuery", "executeSQLQuery"), className, false);
+		}
+		if (className.contains("asen") && className.contains("DetachedQueryExecutorImpl")) {
+			return transform(classfileBuffer, Arrays.asList("execute", "executeOnQueryPool"), className, true);
+		}
+		if (className.contains("CGLIB") || className.contains("$") || className.contains("javassist") || className.contains("asen")) {
+			return classfileBuffer;
+		}
+
+		if (className.contains("server") || className.contains("webservice")) {
+			return transform(classfileBuffer, className, false);
+		}
+
+		ClassReader cr = new ClassReader(classfileBuffer);
+		ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES);
+		org.objectweb.asm.ClassVisitor profiler = new ProfileClassAdapter(cw, className);
+		TraceClassVisitor cv = new TraceClassVisitor(cw, new PrintWriter(System.out));
+		// ClassVisitor cv = new LogMethodClassVisitor(cw, className);
+		// cr.accept(cv, 0);
+		cr.accept(profiler, 0);
+		return cw.toByteArray();
+	}
+
+	private byte[] transform(final byte[] classfileBuffer, final String onlyMethod, final String className, final boolean onlyFirstParam) {
+		return transform(classfileBuffer, Arrays.asList(onlyMethod), className, onlyFirstParam);
+	}
+
+	private byte[] transform(final byte[] classfileBuffer, final String className, final boolean onlyFirstParam) {
+		return transform(classfileBuffer, Collections.EMPTY_LIST, className, onlyFirstParam);
+	}
+
+	private byte[] transform(final byte[] classfileBuffer, final List<String> onlyMethod, final String className,
+			final boolean onlyFirstParam) {
+		ClassPool pool = ClassPool.getDefault();
+		// System.out.println("TRANSFORM " + className);
+		CtClass cl = null;
+		byte[] b = null;
+		try {
+			cl = pool.makeClass(new java.io.ByteArrayInputStream(classfileBuffer));
+			CtBehavior[] methods = cl.getDeclaredBehaviors();
+			for (CtBehavior method : methods) {
+				if (method.isEmpty() == false) {
+					if (!onlyMethod.isEmpty()) {
+						if (onlyMethod.contains(method.getName())) {
+							changeMethod(method, onlyFirstParam);
+						}
+					} else {
+						changeMethod(method, onlyFirstParam);
+					}
+					// System.out.println("TRANSFORM " + methods[i]);
+				}
+			}
+			b = cl.toBytecode();
+
+			// cl.setName("Main");
+
+			// cl.writeFile("c:\\");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (cl != null) {
+				cl.detach();
+			}
+		}
+		return b;
+	}
+
+	private void changeMethod(final CtBehavior method, final boolean onlyFirstParam) throws NotFoundException, CannotCompileException {
+
+		if (method.getModifiers() == Modifier.PROTECTED) {
+			// return;
+		}
+		// method.addLocalVariable("__start", CtClass.longType);
+		// method.addLocalVariable("__stop", CtClass.longType);
+		// method.insertBefore(" __start = System.currentTimeMillis();");
+		// method.insertAfter(" __stop = System.currentTimeMillis() -
+		// __start;");
+		// method.addLocalVariable("__start", CtClass.longType);
+		// method.addLocalVariable("__stop", CtClass.longType);
+		String longName = method.getDeclaringClass().getSimpleName() + "." + method.getName();
+		if (longName.contains("$")) {
+			return;
+		}
+		try {
+			if (onlyFirstParam) {
+				method.insertBefore("System.out.println(\"" + longName + "\");");
+				// method.insertBefore("ProfileSession.opStart(\"" + longName + "\", $1);");
+			} else {
+				method.insertBefore("System.out.println(\"" + longName + "\");");
+				// method.insertBefore("ProfileSession.opStart(\"" + longName + "\", $args);");
+
+			}
+			// method.insertAfter("ProfileSession.opStop(\"" + longName + "\");", true);
+		} catch (CannotCompileException e) {
+			throw new CannotCompileException(longName, e);
+		}
+	}
+}
